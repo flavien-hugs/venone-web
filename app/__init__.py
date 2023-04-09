a6e293b5b2c94df52b65ca9048e9386f704a3406
BEF <geekactux@gmail.com>
2023-04-09 00:19:23 +0000

    update app parameters and packages config

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
-import os
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
-from flask import Flask, render_template
-
-from flask_mail import Mail
-from flask_moment import Moment
-from flask_migrate import Migrate
-from flask_htmlmin import HTMLMIN
-from flask_flatpages import FlatPages
-from flask_sqlalchemy import SQLAlchemy
-
 from config import config
+from flask import Flask
+from flask import render_template
+from flask_flatpages import FlatPages
+from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_minify import Minify
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 
 mail = Mail()
@@ -19,7 +18,7 @@ db = SQLAlchemy()
 moment = Moment()
 migrate = Migrate()
 pages = FlatPages()
-htmlmin = HTMLMIN(remove_comments=False, remove_empty_space=True)
+minify = Minify(html=True, js=True, cssless=True, bypass=["main.*"])
 
 
 def create_app(config_name):
@@ -32,7 +31,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
-    htmlmin.init_app(app)
+    minify.init_app(app)
 
     app.url_map.strict_slashes = False
     app.jinja_env.globals.update(zip=zip)
